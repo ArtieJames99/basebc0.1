15371ec72b7a6ce666ed4bf8d587bb6e826754ae
ArtieJames99 <105764119+ArtieJames99@users.noreply.github.com>
2024-02-17 12:47:46 -0700

    Add files via upload

---
+++ base-localbc/src/blockchain.rs
@@ -0,0 +1,21 @@
+// blockchain.rs
+
+use crate::block::Block; //Import the Block structure from the block.rs module
+
+//Genesis block is the starting block of the block chain with no previous hash value
+//Put in place as the beginning of the blockchain to provide a link for all future blocks
+
+pub fn initialize_blockchain() -> Vec<Block> {
+    // Crate Genisis block 
+    let genesis_block = Block::genesis();
+
+    // Initialize the blockchain with genesis block
+    let mut blockchain = Vec::new();
+    blockchain.push(genesis_block);
+}
+
+// Function returns the genesis block  (the first block in the blockchain)
+pub fn get_genesis_block(blockchain: &[Block]) -> Option<&Block> {
+    // If blockchain not empty, return a reference of the first block
+    blockchain.first();
+}
