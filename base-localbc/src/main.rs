6debf7f1d9caf0a3d57709c6b9a5787a39dc1259
ArtieJames99 <105764119+ArtieJames99@users.noreply.github.com>
2024-02-17 14:00:58 -0700

    Add files via upload

---
+++ base-localbc/src/main.rs
@@ -0,0 +1,27 @@
+//main.rs
+
+mod block; // Import the block.rs module
+mod blockchain; //Imports the blockchain.rs module
+
+use crate::blockchain::get_genesis_block;
+use crate::blockchain::initialize_blockchain;
+use block::Block; // Imports the block module
+
+fn main() {
+    // Initializes the block chain
+    let blockchain = initialize_blockchain();
+
+    //prints the string of the genesis block to console
+    // Get the genesis block from the blockchain
+    if let Some(genesis_block) = get_genesis_block(&blockchain) {
+        // Print the details of the genesis block
+        println!("Genesis Block:");
+        println!("Index: {}", genesis_block.index());
+        println!("Timestamp: {}", genesis_block.timestamp());
+        println!("Data: {}", genesis_block.data());
+        println!("Previous Hash: {}", genesis_block.previous_hash());
+        println!("Hash: {}", genesis_block.hash());
+    } else {
+        println!("No genesis block found.");
+    }
+}
