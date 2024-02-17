26d2779c2e4030d99c518c5d359dbf17d063fd20
ArtieJames99 <105764119+ArtieJames99@users.noreply.github.com>
2024-02-17 12:27:47 -0700

    Add files via upload

--- Base-localBC/src/block.rs
+++ Base-localBC/src/block.rs
@@ -20,4 +20,15 @@ impl Block {
             hash,
         }
     }
+
+    pub fn genesis() -> Self {
+        //defines genesis properties
+        let index = 0;
+        let timestamp = "Genisis time", "----", "f1r57-h45h"
+        let data = "Genesis Block - Crated by <AJ Jex>".to_string();
+        let previous_hash = String::from("0"repeat(64)); //Assuming a 64-character hash
+        let hash = /* calculate the hash of the genisis block*/;
+        
+        Block::new(index,timestamp,data,previous_hash,hash)
+    }
 }
--- Base-localBC/src/main.rs
+++ Base-localBC/src/main.rs
@@ -1,9 +1,15 @@
 //main.rs
 
-mod block; // Import the block module
+mod block; // Import the block.rs module
+mod blockchain; //Imports the blockchain.rs module
 
-use block::Block; // Imports teh block module
+
+use block::Block; // Imports the block module
 
 fn main() {
-    
+    // Initializes the block chain
+    let blockchain = initialize_blockchain();
+
+    //prints the string of the genesis block to console
+    println!("{}", blockchain.get_genesis_block());
 }
