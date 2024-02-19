e76c5ce0d59edad5175583d12b4703e133f303d4
ArtieJames99 <105764119+ArtieJames99@users.noreply.github.com>
2024-02-19 15:35:24 -0700

    Add files via upload

--- rs-chain/src/block.rs
+++ rs-chain/src/block.rs
@@ -29,6 +29,26 @@ impl Block {
         }
     }
     
+    // Hashing  function that takes string input and returns a SHA-256 Hexadecimal string
+    pub fn hash(timestamp u64, last_hash: &str, data: &str) {
+        // Concatenate the timestamp lasthash, and data into a single string
+        let concatenated_string = format! {"{}{}{}", timestamp, last_hash, data};
+    
+        //Hash the string created above using sha
+        let mut hasher = Sha256::new();
+        hasher.update(concatenated_string);
+    
+        // Get hashed value as a byte array
+        let hashed_bytes = hasher.finalize();
+    
+        //convert the byte array into hex string
+        let hashed_string= format!("{:x}", hashed_bytes);
+    
+        // Return the compleated hashed string
+        hashed_string
+    }
+
+
     // Below sets the presets for the genesis block
     pub fn index(&self) -> u32 {
         self.index
@@ -61,15 +81,16 @@ impl Block {
         Block::new(index, timestamp, data, previous_hash, hash)
     }
     //genesis block ends here
-
-
-    //static mineblock function
-    pub fn mine_block(last_block: &Block, data:  &str) -> Block {
-        let index = 1;
-        let timestamp = initialize_timestamp();
-        let last_hash = last_block.hash();
-        let hash = String::from("test-hash");
-
-        Block::new(index, timestamp, data.to_string(), last_hash.to_string(), hash)
-    }
 }
+
+//static mineblock function
+pub fn mine_block(last_block: &Block, data:  &str) -> Block {
+    let index = 1;
+    let timestamp = initialize_timestamp();
+    let last_hash = last_block.hash();
+    let hash = hash(timestamp, lasth_hash, data); //calculates the hash of the new block's data
+
+    Block::new(index, timestamp, data.to_string(), last_hash.to_string(), hash)
+}
+
+
